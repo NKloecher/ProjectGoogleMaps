44394c894a109fae361d82c383e8369957f336ae
mathias hansen <mathiasosthan@gmail.com>
2018-04-23 09:55:08 +0200

    added google maps

--- app/src/main/java/com/example/nicolai/project2/MainActivity.java
+++ app/src/main/java/com/example/nicolai/project2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nicolai.project2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, MapsFragment.class));
     }
 }
